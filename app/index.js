7f1882568532c66586a11b79249b5528479386ef
jaber <srks662@gmail.com>
2022-08-10 01:14:43 +0300

    changed to port 80 node js

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 var ip = require("ip");
 const express = require("express");
 const app = express();
-const port = 9090;
+const port = 80;
 app.get("/", (req, res) => {
   res.send(ip.address());
 });
-app.listen(port, () => console.log("server started" ));
+app.listen(port, () => console.log("server started"));
